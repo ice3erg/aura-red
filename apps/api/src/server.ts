f0a0909e2a5e1b398e491efbef8407d6dd5e7752
ice3erg <164074032+ice3erg@users.noreply.github.com>
2026-03-25 01:59:44 +0300

    Delete apps directory

--- apps/api/src/server.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { createApp } from "./app";
-import { env } from "./config/env";
-
-const app = createApp();
-
-app.listen(env.PORT, () => {
-  console.log(`API running on http://localhost:${env.PORT}`);
-});
